2748f7ccc030ed0babf59195172b9b75f79b8228
chenxiang2022 <114199792+chenxiang2022@users.noreply.github.com>
2022-10-10 22:26:22 -0400

    Update SEC Webcrawler.py
    Changed to include linked text in the output

--- SEC Webcrawler.py
+++ SEC Webcrawler.py
@@ -8,6 +8,7 @@ urls = [seed_url]    #queue of urls to crawl
 seen = [seed_url]    #stack of urls seen so far
 opened = []          #we keep track of seen urls so that we don't revisit them
 contain_charges = []
+text = []
 
 maxNumUrl = 20; #set the maximum number of urls to visit
 # print("Starting with url="+str(urls))
@@ -30,25 +31,30 @@ while len(urls) > 0 and len(contain_charges) < maxNumUrl:
     # ADD THE URLS FOUND TO THE QUEUE url AND seen
     soup = BeautifulSoup(webpage)  #creates object soup
 
-    o_text = soup.get_text()
-    lower_text = o_text.lower()
-    if "charges" in lower_text:
-        contain_charges.append(curr_url)
+    # o_text = soup.get_text()
+    # lower_text = o_text.lower()
+    # if "charges" in lower_text:
+    #     contain_charges.append(curr_url)
     check_url = "https://www.sec.gov/news/press-release"
     # Put child URLs into the stack
     for tag in soup.find_all('a', href = True): #find tags with links
         childUrl = tag['href'] #extract just the link
         o_childurl = childUrl
         childUrl = urllib.parse.urljoin(seed_url, childUrl)
-        # print("seed_url=" + seed_url)
-        # print("original childurl=" + o_childurl)
-        # print("childurl=" + childUrl)
+        
         # print("seed_url in childUrl=" + str(seed_url in childUrl))
         # print("Have we seen this childUrl=" + str(childUrl in seen))
-        if check_url in childUrl and childUrl not in seen:
+        if check_url in childUrl and childUrl not in seen and len(contain_charges) < maxNumUrl:
+            # print("seed_url=" + seed_url)
+            # print("original childurl=" + o_childurl)
+            # print("childurl=" + childUrl)
             #print("***urls.append and seen.append***")
             urls.append(childUrl)
             seen.append(childUrl)
+            linked_text = tag.get_text().lower()
+            if "charges" in linked_text:
+                contain_charges.append(childUrl)
+                text.append(linked_text)
         else:
             #print("######")
             pass
@@ -57,8 +63,12 @@ while len(urls) > 0 and len(contain_charges) < maxNumUrl:
 # print("List of seen URLs:")
 # for seen_url in seen:
 #     print(seen_url)
-for url in contain_charges:
-    print(url)
+print("output:")
+# print(len(contain_charges))
+# print(len(text))
+for k in range(len(contain_charges)):
+    print(contain_charges[k])
+    print(text[k])
 
 # print(len(contain_charges))
 # print(len(urls))
